fee489fe7be7b9bd04c1ab9e8b0d9cd1a512a0c1
seraleman <seraleman1204@gmail.com>
2022-09-28 20:45:31 -0500

    Se modulariza lógica y se crea promesa

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-const fs = require('fs')
-
-const base = 78
+const { crearArchivo } = require('./helpers/multiplicar')
 
 console.clear()
-console.log('=================')
-console.log(`   Tabla del ${base}`)
-console.log('=================')
 
-let salida = ''
+const base = 6
 
-for (let i = 1; i <= 10; i++) {
-  salida += `${base} x ${i} = ${base * i}\n`
-}
-
-console.log(salida)
-
-fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
-  if (err) throw err
-  console.log(`Tabla del ${base} creada`)
-})
+crearArchivo(base)
+  .then((nombreArchivo) => console.log(nombreArchivo, 'creado'))
+  .catch(console.error)
