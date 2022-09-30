43a6b3bc042fba79f417adc3a588cc5de2f177e0
seraleman <seraleman1204@gmail.com>
2022-09-30 17:23:39 -0500

    Reinstalando colores y tarea

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const { crearArchivo } = require('./helpers/multiplicar')
+require('colors')
 const argv = require('./config/yargs')
+
+/**
+ * Jugar con los colores
+ */
 
 console.clear()
 
 crearArchivo(argv.b, argv.l)
-  .then((nombreArchivo) => console.log(nombreArchivo, 'creado'))
+  .then((nombreArchivo) => console.log(nombreArchivo.green, 'creado'))
   .catch(console.error)
